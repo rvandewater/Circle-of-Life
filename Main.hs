927a076868bd1ac4bf8e7af8c1f7999f72d48966
RobinvdwUU <r.p.vandewater@students.uu.nl>
2017-10-12 16:56:19 +0200

    Prachtig

--- Main.hs
+++ Main.hs
@@ -4,6 +4,6 @@ drawing :: Picture
 drawing = (pictures [  translate 0 (-300) (Circle 80), translate (-500) 0 (Text "The circle of life")])
 main = animate (InWindow "Nice Window" (200, 200) (10, 10)) white (\t ->  
     (pictures [ (translate 0 (-300) (Circle 80)),
-    (rotate (60*t) (translate (-500) 0 (Text "The circle of life")))]))
+    (scale (1*t) (1*t) (Text "The circle of life"))]))
     
 
