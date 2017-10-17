50eed1ffc3f9544b689c74a4db70fc3c355451ab
RobinvdwUU <r.p.vandewater@students.uu.nl>
2017-10-17 15:11:37 +0200

    Een bewegende cirkel!
    Een cirkel beweegt nu met de wasd knoppen

--- src/Controller.hs
+++ src/Controller.hs
@@ -1,5 +1,6 @@
 -- | This module defines how the state changes
 --   in response to time and user input
+{-# language NamedFieldPuns #-}
 module Controller where
 
 import Model
@@ -11,12 +12,6 @@ import System.Random
 -- | Handle one iteration of the game
 step :: Float -> GameState -> IO GameState
 step secs gstate
-  | elapsedTime gstate + secs > nO_SECS_BETWEEN_CYCLES
-  = -- We show a new random number
-    do randomNumber <- randomIO
-       let newNumber = abs randomNumber `mod` 10
-       return $ GameState (ShowANumber newNumber) 0
-  | otherwise
   = -- Just update the elapsed time
     return $ gstate { elapsedTime = elapsedTime gstate + secs }
 
@@ -25,7 +20,14 @@ input :: Event -> GameState -> IO GameState
 input e gstate = return (inputKey e gstate)
 
 inputKey :: Event -> GameState -> GameState
-inputKey (EventKey (Char c) _ _ _) gstate
-  = -- If the user presses a character key, show that one
-    gstate { infoToShow = ShowAChar c }
+inputKey (EventKey (Char c) _ _ _) gstate@(GameState {playerPos})
+   | c == 'w' = gstate { playerPos = Position curposx (curposy+10)  }
+   | c == 'a' = gstate { playerPos = Position (curposx-10) curposy  }
+   | c == 's' = gstate { playerPos = Position curposx (curposy-10)  }
+   | c == 'd' = gstate { playerPos = Position (curposx + 10) curposy  }
+     where xpos (Position x y) = x
+           ypos (Position x y) = y
+           curposx = xpos playerPos
+           curposy = ypos playerPos
+    -- If the user presses a specific character key, move the player
 inputKey _ gstate = gstate -- Otherwise keep the same
--- src/Model.hs
+++ src/Model.hs
@@ -1,18 +1,23 @@
 -- | This module contains the data types
 --   which represent the state of the game
 module Model where
+         
+data Player = Player { pos :: Position}
 
-data InfoToShow = ShowNothing
-                | ShowANumber Int
-                | ShowAChar   Char
+data Position = Position { xpos :: Int, ypos :: Int }
+
+data Move = Move { xmov :: Int, ymov :: Int }
 
 nO_SECS_BETWEEN_CYCLES :: Float
 nO_SECS_BETWEEN_CYCLES = 5
 
+updatePos :: Player -> Move -> Player
+updatePos (Player (Position xpos ypos)) (Move xmov ymov) = Player (Position (xpos+xmov) (ypos+ymov))
+
 data GameState = GameState {
-                   infoToShow  :: InfoToShow
-                 , elapsedTime :: Float
+                   elapsedTime :: Float
+                 , playerPos :: Position
                  }
 
 initialState :: GameState
-initialState = GameState ShowNothing 0
+initialState = GameState 0 (Position 0 0)
