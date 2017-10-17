7327ffbeee1bc4fe9a41f654534eb041ad50f0fa
RobinvdwUU <r.p.vandewater@students.uu.nl>
2017-10-17 11:56:33 +0200

    Mooie update

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,4 +1,4 @@
-# Revision history for HaskellGame
+# Revision history for CoL
 
 ## 0.1.0.0  -- YYYY-mm-dd
 
--- Main.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-import Graphics.Gloss
-drawing :: Picture
-drawing = (pictures [  translate 0 (-300) (Circle 80), translate (-500) 0 (Text "The circle of life")])
-main = animate (InWindow "Nice Window" (200, 200) (10, 10)) white (\t ->  
-    (pictures [ (translate 0 (-300) (Circle 80)),
-    (scale (1*t) (1*t) (Text "The circle of life"))]))
-    
-
