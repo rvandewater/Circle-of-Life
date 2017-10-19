443b637f0a0132296f8f1a10bef786c6cc404e60
devanunen <32739546+devanunen@users.noreply.github.com>
2017-10-19 21:43:30 +0200

    Formatting changes
    Paar layout/formatting veranderingen en added comments

--- src/Controller.hs
+++ src/Controller.hs
@@ -11,23 +11,24 @@ import System.Random
 
 -- | Handle one iteration of the game
 step :: Float -> GameState -> IO GameState
-step secs gstate@(GameState { paused })
-  = -- Just update the elapsed time
-   if (not paused) then return $ gstate { elapsedTime = elapsedTime gstate + secs, playerPos = (movementUpdate (keys gstate) (playerPos gstate)) }
-   else return gstate
+step secs gstate@GameState { paused }
+  = if (not paused) then return $ gstate { elapsedTime = elapsedTime gstate + secs                          --update time
+                                           ,playerPos = (movementUpdate (keys gstate) (playerPos gstate)) } --update player position
+                    else return   gstate
 
-
+-- | Updating Position depending on keys pressed
 movementUpdate :: KeysPressed -> Position -> Position
 movementUpdate (KeysPressed w a s d) =  boolupdate w (Move 0 movementSpeed). 
-                                      boolupdate a (Move (- movementSpeed) 0).
-                                      boolupdate s (Move 0 (- movementSpeed)).
-                                      boolupdate d (Move movementSpeed 0)
-                              where boolupdate True mov = updatePos mov
-                                    boolupdate False _ = id
+                                        boolupdate a (Move (- movementSpeed) 0).
+                                        boolupdate s (Move 0 (- movementSpeed)).
+                                        boolupdate d (Move movementSpeed 0)
+                                where     boolupdate True mov = updatePos mov
+                                          boolupdate False _ = id
 -- | Handle user input
 input :: Event -> GameState -> IO GameState
 input e gstate = return (inputKey e gstate)
 
+-- | Handle input of a key
 inputKey :: Event -> GameState -> GameState
 {-inputKey (EventKey (Char c) _ _ _) gstate@(GameState {playerPos})
    | c == 'w' = gstate { playerPos = (updatePos playerPos (Move 0 movementSpeed))  }
@@ -38,20 +39,19 @@ inputKey :: Event -> GameState -> GameState
            ypos (Position x y) = y
            curposx = xpos playerPos
            curposy = ypos playerPos -}
-inputKey (EventKey (Char 'p') Down _ _) gstate@(GameState { keys, paused })= gstate {paused = not paused}
-inputKey (EventKey (Char c) _ _ _) gstate@(GameState { keys, paused })= gstate {keys = (updatePress c keys )}
+inputKey (EventKey (Char 'p') Down _ _) gstate@GameState { keys, paused }= gstate {paused = not paused}
+inputKey (EventKey (Char c) _ _ _) gstate@GameState { keys, paused }= gstate {keys = (updatePress c keys )}
 {-
 inputKey (EventKey  _ _ _ _) gstate@(GameState {playerPos}) = 
   gstate { playerPos = (updatePos playerPos (Move 0 movementSpeed))  -}
     -- If the user presses a specific character key, move the player
-
 inputKey _ gstate = gstate -- Otherwise keep the same 
 
+-- | Updating keyspressed depending on which key is pressed
 updatePress :: Char -> KeysPressed -> KeysPressed
-updatePress 'w' keys@(KeysPressed {w}) = keys { w = not w }
-updatePress 'a' keys@(KeysPressed {a}) = keys { a = not a }
-updatePress 's' keys@(KeysPressed {s}) = keys { s = not s }
-updatePress 'd' keys@(KeysPressed {d}) = keys { d = not d }
-
-updatePress _ x = x
+updatePress 'w' keys@KeysPressed {w} = keys { w = not w }
+updatePress 'a' keys@KeysPressed {a} = keys { a = not a }
+updatePress 's' keys@KeysPressed {s} = keys { s = not s }
+updatePress 'd' keys@KeysPressed {d} = keys { d = not d }
+updatePress  _  x                    = x
 
--- src/View.hs
+++ src/View.hs
@@ -9,7 +9,7 @@ view :: GameState -> IO Picture
 view = return . viewPure
 
 viewPure :: GameState -> Picture
-viewPure (GameState _ _ _ True) = translate (-200) 0 (color green (Text "Paused"))
-viewPure (GameState s (Position xpos ypos) _ _) = (translate (fromIntegral xpos) (fromIntegral ypos) (color green (thickCircle 20 40)))
+viewPure (GameState _ _                     _ True) =  translate (-200) 0 (color green (Text "Paused"))
+viewPure (GameState s (Position xpos ypos)  _ _)    = (translate (fromIntegral xpos) (fromIntegral ypos) (color green (thickCircle 20 40)))
 
 
