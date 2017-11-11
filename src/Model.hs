a6933cf79179cf0ed6530cd89a7434c32d0597c9
RobinvdwUU <r.p.vandewater@students.uu.nl>
2017-11-11 18:54:53 +0100

    Comments

--- src/Model.hs
+++ src/Model.hs
@@ -26,6 +26,7 @@ data GameState = GameState {
 
 data Bullet = Bullet { position:: Position, kind :: BulletType, frame :: Float}
 
+--Different screens for different points
 data GameScreen = MainMenu | GameOver| WriteScore Bool | ReadScore | PlayGame | DifficultySelect | LevelSelect | PausedGame | HighScores | NoScreen 
   deriving (Eq)
 
@@ -78,8 +79,8 @@ outOfBounds (Position posx posy) (HitBox width height) =  not validPosition
 
 -- Typeclass ship                        
 class Ship k where 
-  isHit     :: k      -> Bullet -> Maybe Float
-  getDamage :: Float  -> k      -> k
+  isHit     :: k      -> Bullet -> Maybe Float      --Wether the ship is hit by a bullet
+  getDamage :: Float  -> k      -> k                -- Subtract damage and set animation
 
 -- Instance for player
 instance Ship Player where 
@@ -87,24 +88,27 @@ instance Ship Player where
                                                                                            | otherwise                                 = Nothing
   getDamage dmg player@Player { health} | dmg <= 0  = player
                                         | otherwise = player {health = health - dmg, hitAnim = 1}
+--Instance for Enemies
 instance Ship Enemy where
   isHit enemy@Enemy {epos, ehitbox, ehealth} (Bullet bulletpos (BulletType _ size dmg _) _)    | collision (ehitbox, epos) (size, bulletpos) = Just dmg
                                                                                                | otherwise                                   = Nothing
   getDamage dmg enemy@Enemy { ehealth} | dmg <= 0 = enemy
                                        | otherwise =  enemy {ehealth = ehealth - dmg, eHitAnim = 1}
                                          
-
+--Checks if Ships are hit by a list of bullets
 shipsHit :: Ship k => [Bullet] -> [k] -> ([k],[Bullet] )
 shipsHit bullets = foldl oneship ( [], bullets) where
     oneship :: Ship k => ([k],[Bullet]) -> k -> ([k],[Bullet])
     oneship (shiplist, bullets') ship = (ship':shiplist, bull) 
         where (bull, ship')= shipHit  bullets' ship 
-                                      
+
+--Checks if one ship is hit by a list of bullets                      
 shipHit :: Ship k => [Bullet] -> k -> ([Bullet], k)
 shipHit bullets k = (filter checker bullets,getDamage totaldam k)
           where totaldam = sum (mapMaybe (isHit k) bullets)
                 checker thisBull@(Bullet pos (BulletType speed size bulletDamage _ ) _) = isNothing(isHit k thisBull)
 
+--Checks if a certain powerup is picked up
 getPower :: Player -> PowerUp -> (Player,Maybe PowerUp)
 getPower player@Player {pos, hitbox, health} pu@(Health hp ppos hb)        | collision (hitbox, pos) (hb, ppos) = (player{health = health + hp},Nothing)
                                                                            | otherwise                          = (player, Just pu )
@@ -116,30 +120,33 @@ getPower player@(Player {pos, hitbox,fireRate }) pu@(FireRate fr ppos hb)  | col
 getPower player@(Player {pos, hitbox, health, bullet = bullet@BulletType {damage}}) pu@(Damage dm ppos hb)  | collision (hitbox, pos) (hb, ppos) && (damage < 10) = (player{bullet = bullet{damage=damage+dm}}, Nothing)
                                                                                                             | collision (hitbox, pos) (hb, ppos) && (damage >= 10) = (player, Nothing)
                                                                                                             | otherwise = (player, Just pu )
+-- Checks if player collides with enemy and gives damage.
 enemyColl :: Player -> Enemy -> (Float, Enemy)
 
 enemyColl player@(Player {pos, hitbox, health}) enemy@(Enemy {epos, ehitbox, ehealth})    | collision (hitbox, pos) (ehitbox, epos) = (collDamage, enemy {ehealth= ehealth-collDamage, eHitAnim=1})
                                                                                           | otherwise = (0, enemy)
-
+-- Kill/ animate  an enemy if hitpoints are to low 
 enemyKill :: Enemy -> Maybe Enemy
 enemyKill enemy@Enemy{ehealth, killAnim}                            | ehealth > 0 = Just enemy
                                                                     | killAnim == 0 = Just enemy{killAnim = scoreAnim}
                                                                     | killAnim < 0 = Nothing
                                                                     | otherwise = Just enemy
+--Updates the score for killing an enemy
 enemyScore :: Enemy -> Int
 enemyScore enemy@Enemy{ehealth, killpoints, killAnim}               | ehealth > 0 = 0
                                                                     | killAnim == scoreAnim = killpoints
                                                                     | otherwise = 0
-
+--Checks if enemy can fire something
 enemyShoot ::  Float -> Enemy -> (Maybe Bullet,Enemy)
 enemyShoot secs enem@Enemy{eBullet = eBullet@BulletType{size = HitBox bsx bsy }, eLastFire, epos = (Position xp yp), ehitbox= (HitBox x y)} 
       | enemyCanShoot enem = (Just (Bullet (Position xp (yp- (fromIntegral y) `div` 2 - (bsx `div` 2 ))) eBullet 0) , enem{ eLastFire = 0}  )
       | otherwise = (Nothing,  enem{ eLastFire = secs + eLastFire})
 
+--Checks if enemy can shoot
 enemyCanShoot :: Enemy -> Bool                                            
 enemyCanShoot Enemy{efireRate, eLastFire} | efireRate < eLastFire = True
                                           | otherwise             = False
-     
+--Collision detection 
 collision:: (HitBox, Position) -> (HitBox, Position) -> Bool
 collision (HitBox w1 h1, Position x1 y1) (HitBox w2 h2, Position x2 y2) = ((x1 - b1) < (x2 + b2)) &&
                                                                           ((x1 + b1) > (x2 - b2)) &&
@@ -151,7 +158,7 @@ collision (HitBox w1 h1, Position x1 y1) (HitBox w2 h2, Position x2 y2) = ((x1 -
                                                                                 v1 = (h1 `quot` 2)
                                                                                 v2 = (h2 `quot` 2)                                                                               
 -- ********************* CONSTANTS ***************
--- Starting phase
+-- Phase of the game that starts
 initialState :: StdGen -> GameState
 initialState = GameState 
                          0
@@ -168,6 +175,8 @@ initialState = GameState
                          ""
                          "could not retrieve score"
 
+--Select enemy function which selects from a list with the given difficulty
+
 selectEnemy :: Int -> Int -> Enemy
 --                         position   hitbox           fireRate  bullettype                lastfire  health    ai  speed killpoints  HitAnim DeathAnim
 selectEnemy d sel = [Enemy enemySpawn (HitBox 50 50)   2         (difMod standardEBullet d)  0         5       0   2     1           0       0
@@ -176,6 +185,7 @@ selectEnemy d sel = [Enemy enemySpawn (HitBox 50 50)   2         (difMod standar
                     ,Enemy enemySpawn (HitBox 50 50)   2         (difMod fastEBullet d)      0         5       3   2     10          0       0
                     ,Enemy enemySpawn (HitBox 50 60)   1         (difMod fastEBullet d)      0         5       4   2     20          0       0 ]!!sel
 
+-- Change damage of bullet depending on difficulty
 difMod :: BulletType -> Int -> BulletType
 difMod bull@BulletType{damage} mod = bull{damage = damage * (fromIntegral mod)}
 
@@ -187,7 +197,7 @@ fastEBullet       = (BulletType (Move 0 (-13))  bulletHitBox    2             (c
 heavyEBullet      = (BulletType (Move 0 (-5 ))  (HitBox 20 20)  20            (color (dark(dark           red))  (circleSolid 10)))
 rapidfireEBullet  = (BulletType (Move 0 (-8 ))  bulletHitBox    1             (color                  magenta    (circleSolid 5 )))
 
--- MoventSpeed of the player
+-- Rest of the constants are self explanatory
 movementSpeed :: Int
 movementSpeed = 5
 
